1ee9356614b434fa3cbe61e881fc64799e3b7c51
Nelson Vilte <nelsonvilte@gmail.com>
2020-12-26 13:41:41 -0300

    Se actualizaron componentes: destino-viaje y lista-destino

--- angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DestinoViaje} from './../models/destino-viaje.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -6,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-   destinos:string[];
-  constructor() { 
-    this.destinos=['Cachi','Cafayate','San Antonio de los Cobres','Quijano']
+   destinos:DestinoViaje[];
+   constructor() { 
+    this.destinos=[];
   }
 
   ngOnInit(): void {
+  }
+
+  guardar(nombre:string,url:string):boolean{
+    this.destinos.push(new DestinoViaje(nombre,url));
+/*     console.log(this.destinos) ;
+ */    return false;
   }
 
 }
