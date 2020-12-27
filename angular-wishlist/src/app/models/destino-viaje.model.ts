3bff88fbdc154519f1a890f4c9318461acfef444
Nelson Vilte <nelsonvilte@gmail.com>
2020-12-26 21:48:24 -0300

    Prototipado y personalización de vistas: ngfor - ngif - ngNonBindable - ngSwitch con ngSwitchCase y ngSwitchDefault

--- angular-wishlist/src/app/models/destino-viaje.model.ts
+++ angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -1,7 +1,11 @@
 export class DestinoViaje {
   private selected:boolean;
+  public servicios:string[];
 
-  constructor(public nombre:string, public u:string){}
+  constructor(public nombre:string, public u:string){
+      this.servicios = ['pileta','desayuno'];
+  }
+  
   isSelected():boolean{
       return this.selected;
   }
