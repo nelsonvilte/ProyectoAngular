a76b2eb3691447e8a332fc8b4d43311f9b9d8955
Nelson Vilte <nelsonvilte@gmail.com>
2020-12-28 12:19:00 -0300

    Validaciones personalizadas

--- angular-wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ angular-wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { DestinoViaje } from '../models/destino-viaje.model';
-import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -10,11 +10,15 @@ import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   fg: FormGroup;
+  minLongitud = 5;
 
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter();
     this.fg = fb.group({
-      nombre:['', Validators.required],
+      nombre:['', Validators.compose([
+        Validators.required,
+        this.nombreValidatorParametrizable(this.minLongitud)
+      ])],
       url:['']
     });
     //observador de tipeo
@@ -32,5 +36,24 @@ export class FormDestinoViajeComponent implements OnInit {
     this.onItemAdded.emit(d);
     return false;
 
+  }
+
+
+  nombreValidator(control: FormControl): {[s:string]:boolean}{
+    let l = control.value.toString().trim().length;
+    if (l>0 && l<5){
+      return{ invalidNombre: true};
+    }
+    return null;
+  }
+
+  nombreValidatorParametrizable(minLong: number): ValidatorFn{
+    return(control: FormControl):{[s:string]:boolean} | null =>{
+      const l = control.value.toString().trim().length;
+      if(l > 0 && l < minLong){
+        return{ minLongNombre:true};
+      }
+      return null;
+    }
   }
 }
